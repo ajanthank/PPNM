8a12620ed1203e2e5e07c4c3b99f9fa3c8cefe7d
Ajanthan K. Kumarasamy <ajanthank.kumarasamy@Ajanthans-MacBook-Pro.local>
2021-03-03 22:54:41 +0100

    Commit of gsl_integ now with solution to the bessel function

--- Exercise/gsl-integ/main.c
+++ Exercise/gsl-integ/main.c
@@ -27,7 +27,29 @@ double myG (double z){
   gsl_integration_workspace_free (w);
   return result;
 }
+double bessel(double x, void * param){
+  double t = *(double*)param;
+  double bessel=1.0/M_PI *cos(x-t*sin(x));
+  return bessel;
+}
+double mybessel(){
+  int limit = 1000;
+  gsl_integration_workspace *w = gsl_integration_workspace_alloc (limit);
+  FILE* DataBessel=fopen("DataBessel.txt","w");
+  for(double t=0;t<=20;t+=1.0/100){
+    gsl_function H;
+    H.function = &bessel;
+    H.params = &t;
 
+    double result,error;
+
+    gsl_integration_qags (&H, 0, M_PI, 1e-8, 1e-8, limit, w, &result, &error);
+    fprintf(DataBessel,"%10g %10g\n", t, result);
+  }
+  gsl_integration_workspace_free(w);
+  fclose(DataBessel);
+  return 0;
+}
 
 int main (){
   int limit = 1000;
@@ -48,6 +70,8 @@ int main (){
   for (double x= -3; x<=3;x+=1.0/100){
     fprintf(datab,"%10g %10g\n",x,myG(x));
   }
+
+  mybessel();
 
   gsl_integration_workspace_free (w);
   fclose(datab);
